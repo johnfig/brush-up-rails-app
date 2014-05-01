7b6e6de0eaa5a0ee812aa5c49ca3cd8f1c15899d
John Figueiredo <john@johnfigueiredo.com>
2014-05-01 08:50:08 -0700

    Add and test validations for password and full_name

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,9 @@ attr_accessor :full_name
 # Associations
 
 # Validations
+validates :first_name, :presence => true
+validates :last_name, :presence => true
+validates :password, length: { minimum: 6 }
 
 # Callbacks
 before_save :set_full_name
@@ -12,7 +15,6 @@ before_save :set_full_name
 private
 
   def set_full_name
-    debugger
     self.full_name = "#{self.first_name} #{self.last_name}"
   end
 end
--- spec/models/users_spec.rb
+++ spec/models/users_spec.rb
@@ -1,1 +1,22 @@
-users_spec.rb
+require 'spec_helper'
+
+describe User do
+  let(:user)  { FactoryGirl.create :user }
+
+  context "Create User" do
+    it "User is created with correct attributes" do
+      user.first_name.should == "John"
+    end
+
+    it "should set full name upon save" do
+      user.full_name.should == "John Fig"
+    end
+
+    it "should fail to save if length is below 6" do
+      ap user
+      user.update(password: "wrong")
+      ap user
+      user.password.should == "password"
+    end
+  end
+end
